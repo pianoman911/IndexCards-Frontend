2e1d27bb5f699f053345b9ee53414638f2a471bd
pianoman911 <pianofinn13@gmail.com>
2023-03-21 14:58:02 +0100

    Cleaner card rendering, better controls

--- card.js
+++ card.js
@@ -2,8 +2,8 @@ function renderCard(id, question) {
     document.getElementById('card').innerHTML += ' <div class="flip-card-inner" id="flip-card-inner">\n' +
         '    <div class="flip-card-front">' +
         '      <label for="question" class="question"><b>#' + id + ' - Frage:</b></label>\n ' +
-        ' <div class="front-container">\n' +
-        '       <div class="front-content">\n' +
+        ' <div class="back-container">\n' +
+        '       <div class="back-content">\n' +
         '\n' + question + '\n' +
         '       <br>' +
         '        <input id="answer" type="text" placeholder="Antwort" name="anwort" required onkeypress="clickCard(event)">' +
@@ -14,6 +14,7 @@ function renderCard(id, question) {
         '    </div>\n' +
         '  </div>';
     setCookie("card", id, 99999);
+    deleteCookie("flipped");
 }
 
 function onBack() {
@@ -66,41 +67,46 @@ function setCard() {
 
 function clickCard(e) {
     if (e.keyCode === 13) {
-        checkSession();
-        var id = getCookie("card");
-        var input = document.getElementById("answer").value;
-        var json = {
-            "session": getCookie("session"),
-            "id": id,
-            "input": input
-        };
-        var jsonStr = JSON.stringify(json);
-        var xhr = new XMLHttpRequest();
-        xhr.open("POST", "http://localhost:8080/api/cards/done", true);
-        xhr.onreadystatechange = function () {
-            if (xhr.readyState === 4) {
-                if (xhr.status === 200) {
-                    var json = JSON.parse(xhr.responseText);
-                    if (json.time != null) {
-                        var time = json.time;
-                        var others = json.others;
-                        var correct = json.correct;
+        if (getCookie("flipped") != null) {
+            document.location.reload()
+        } else {
+            checkSession();
+            setCookie("flipped", true);
+            var id = getCookie("card");
+            var input = document.getElementById("answer").value;
+            var json = {
+                "session": getCookie("session"),
+                "id": id,
+                "input": input
+            };
+            var jsonStr = JSON.stringify(json);
+            var xhr = new XMLHttpRequest();
+            xhr.open("POST", "http://localhost:8080/api/cards/done", true);
+            xhr.onreadystatechange = function () {
+                if (xhr.readyState === 4) {
+                    if (xhr.status === 200) {
+                        var json = JSON.parse(xhr.responseText);
+                        if (json.time != null) {
+                            var time = json.time;
+                            var others = json.others;
+                            var correct = json.correct;
 
-                        var html;
-                        if (correct == null) {
-                            html = 'Falsch';
-                            document.getElementById('flip-card-back').style.backgroundColor = "red"
-                        } else {
-                            html = 'correct';
-                            document.getElementById('flip-card-back').style.backgroundColor = "lawngreen"
+                            var html;
+                            if (correct == null) {
+                                html = 'Falsch';
+                                document.getElementById('flip-card-back').style.backgroundColor = "red"
+                            } else {
+                                html = 'correct';
+                                document.getElementById('flip-card-back').style.backgroundColor = "lawngreen"
+                            }
+                            document.getElementById('flip-card-back').innerHTML += html;
+                            document.getElementById('flip-card-inner').style.transform = "rotateY(180deg)";
                         }
-                        document.getElementById('flip-card-back').innerHTML += html;
-                        document.getElementById('flip-card-inner').style.transform = "rotateY(180deg)";
                     }
                 }
             }
+            xhr.setRequestHeader("Content-Type", "application/json");
+            xhr.send(jsonStr);
         }
-        xhr.setRequestHeader("Content-Type", "application/json");
-        xhr.send(jsonStr);
     }
 }
